c76b423f349980ed60e4e8feada7b2af1539d26e
Leroyalle <nikolajmelonov@yandex.ru>
2024-10-29 02:21:36 +0300

    фикс верстки

--- shared/components/shared/media-page/content/media-page-actions.tsx
+++ shared/components/shared/media-page/content/media-page-actions.tsx
@@ -26,7 +26,7 @@ export const MediaPageActions: React.FC<Props> = ({
   });
 
   return (
-    <div ref={ref} className={cn('w-fit flex items-center gap-4', className)}>
+    <div ref={ref} className={cn('w-fit flex items-center gap-2', className)}>
       <Button variant={'secondary'} className="py-6 px-8 text-lg">
         Смотреть {isSeries ? 'сериал' : 'фильм'}
       </Button>
@@ -36,15 +36,20 @@ export const MediaPageActions: React.FC<Props> = ({
       <Button onClick={onClickAddFavorites} variant="ghost" className="py-6 px-4">
         <Heart className={cn(liked && ' fill-white')} />
       </Button>
-      <Button variant="ghost" className="py-6 px-4" onClick={() => setLinkIsOpened(!linkIsOpened)}>
-        <Forward />
-      </Button>
-      <div
-        className={cn(
-          'absolute left-64 bottom-12 w-[250px] transition-all opacity-0 invisible',
-          linkIsOpened && 'opacity-1 visible',
-        )}>
-        <ShareLink items={shareLinkData} />
+      <div>
+        <Button
+          variant="ghost"
+          className="py-6 px-4"
+          onClick={() => setLinkIsOpened(!linkIsOpened)}>
+          <Forward />
+        </Button>
+        <div
+          className={cn(
+            'absolute mt-2 left-64 w-[250px] transition-all opacity-0 invisible',
+            linkIsOpened && 'opacity-1 visible',
+          )}>
+          <ShareLink items={shareLinkData} />
+        </div>
       </div>
     </div>
   );
